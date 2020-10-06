836efcda7cacecaed8953783b484a4ec2a8ae475
Bria <calypsomatic@gmail.com>
2020-10-06 16:56:21 -0400

    Last attempt at Grid, a bit of overlap with next step, probably not going to use

--- Grid.js
+++ Grid.js
@@ -5,18 +5,41 @@ import L from "leaflet";
 
 class Grid extends GridLayer {
 
+  constructor(props) {
+    super(props);
+    console.log("constructor:")
+    console.log(props);
+    this.state = {
+      polygons: props.poly,
+      map: props.leaflet.map
+    }
+    console.log(this.state);
+  }
+
+  //taken from here: https://stackoverflow.com/questions/217578/how-can-i-determine-whether-a-2d-point-is-within-a-polygon
+  pnpoly(nvert, verts, testx, testy) {
+    var i, j, c = 0;
+    for (i = 0, j = nvert-1; i < nvert; j = i++) {
+      if ( ((verts[i][1]>testy) != (verts[j][1]>testy)) &&
+        (testx < (verts[j][0]-verts[i][0]) * (testy-verts[i][1]) / (verts[j][1]-verts[i][1]) + verts[i][0]) ){
+          c = !c;
+        }
+      }
+      return c;
+    }
+
   createLeafletElement() {
 
     // const poly = this.props.poly;
-    this.state = {
-      polygons: this.props.poly,
-      map: this.props.map
-    };
+    // this.state = {
+    //   polygons: this.props.poly,
+    //   map: this.props.map
+    // };
 
     const Layer = L.GridLayer.extend({
       createTile: (coords) => {
-        console.log("create tiles");
-        console.log(this.state);
+        // console.log("create tiles");
+        // console.log(this.state);
         var tile = L.DomUtil.create('canvas', 'leaflet-tile');
         var ctx = tile.getContext('2d');
         var size = this.leafletElement.getTileSize()
@@ -26,31 +49,21 @@ class Grid extends GridLayer {
         var nwPoint = coords.scaleBy(size)
 
         // calculate geographic coordinates of top left tile pixel
-        var nw = this.state.map.leafletElement.unproject(nwPoint, coords.z)
+        // var nw = this.state.map.leafletElement.unproject(nwPoint, coords.z)
+        var nw = this.state.map.unproject(nwPoint, coords.z)
 
-        console.log(this.props.poly)
-        console.log(nw);
+        // console.log(this.props.poly)
+        // console.log(nw);
 
-        var minX = this.props.poly[ 0 ][0];
-        var maxX = this.props.poly[ 0 ][0];
-        var minY = this.props.poly[ 0 ][1];
-        var maxY = this.props.poly[ 0 ][1];
-        for (var i = 1 ; i < this.props.poly.length ; i++ )    {
-         var q = this.props.poly[i];
-         console.log(q);
-         minX = Math.min( q[0], minX );
-         maxX = Math.max( q[0], maxX );
-         minY = Math.min( q[1], minY );
-         maxY = Math.max( q[1], maxY );
-       }
+       var xs = []
 
-       console.log(minX, maxX, minY, maxY);
-       if ( nw.lat < minX || nw.lat > maxX || nw.lng < minY || nw.lng > maxY )
+       if (this.pnpoly(this.props.poly.length, this.props.poly, nw.lat, nw.lng))
        {
-        ctx.fillStyle = "rgba(192, 192, 192, 0.9)";
+         ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
+
         }
 else {
-  ctx.fillStyle = "rgba(150, 102, 92, 0.3)";
+  ctx.fillStyle = "rgba(192, 192, 192, 0.9)";
 
 }
 
--- MapView.js
+++ MapView.js
@@ -1,37 +1,130 @@
 import React, { Component, useState } from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup, Polygon, SVGOverlay, LayerGroup, Circle, FeatureGroup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+// import './App.css';
 import Geolocation from '@react-native-community/geolocation';
+import L from 'leaflet';
+import findIntersections from './map.js';
+import Grid from './Grid.js';
+// import {storeData, getMyObject} from './storage.js';
+
+var bigInt = require("big-integer");
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+// const storeData = async (values) => {
+//   try {
+//     console.log("storing");
+//     const jsonValue = JSON.stringify(values)
+//     console.log(jsonValue);
+//     await AsyncStorage.setItem('@storage_Key', jsonValue)
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+//
+//
+// const getMyObject = async () => {
+//   try {
+//     const jsonValue = await AsyncStorage.getItem('@storage_Key')
+//     console.log(jsonValue);
+//     return jsonValue != null ? JSON.parse(jsonValue) : null
+//   } catch(e) {
+//     console.log(e)
+//   }
+// }
 
 class MapView extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      markers: [],
+      polygons: null,
+      bounds: null
+    }
+  }
+
+
+  componentDidMount() {
     Geolocation.getCurrentPosition(position => {
       this.setState({
         currentLocation: { lat: position.coords.latitude, lng: position.coords.longitude },
           zoom: 14,
-      })
-    });
+          error: null,
+          isLoaded: false
+      });
+      findIntersections(this.state.currentLocation)
+        .then(res => {
+          console.log(res);
+          this.setState({ polygons: res.polygon, markers: res.markers });
+          // storeData(res.polygon)
+        });
+   });
+
   }
 
+  renderGrid(){
+    if (this.state.polygons){
+      console.log(this.refs.map);
+      return <Grid opacity="0.9" poly={this.state.polygons} map={this.refs.map}/>
+    }
+  }
+
+  // renderPolygon(){
+  //   if (this.state.polygons){
+  //       getMyObject().then( res => console.log(res));
+  //       return <Polygon fillColor="purple" positions={this.state.polygons} stroke={false}/>
+  //     }
+  // }
 
   render() {
+
     if (!this.state) {
       return <div>Loading</div>
     }
 
+    // console.log(this);
     //TODO: fix attribution.  given here: http://maps.stamen.com/#toner/12/37.7706/-122.3782
 
     return (
-      <Map center={this.state.currentLocation} zoom={this.state.zoom}>
+      <Map ref="map" center={this.state.currentLocation} zoom={this.state.zoom}>
         <TileLayer
           url="http://tile.stamen.com/toner/{z}/{x}/{y}.png"
           attribution="Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL."
         />
-
+        <Marker position={this.state.currentLocation}>
+        <Popup> You are here </Popup>
+        </Marker>
+        {
+          this.renderGrid()
+        }
       </Map>
     );
   }
 }
 
 export default MapView;
+
+
+// {this.state.markers.map((marker) => (
+//             <Marker position={marker.position} key={marker.position.lat + marker.position.lng}>
+//             <Popup> {marker.label} </Popup>
+//             </Marker>
+//         ))}
+
+
+// <Map ref="map" center={this.state.currentLocation} zoom={this.state.zoom}>
+// <LayerGroup>
+// <Grid/>
+// </LayerGroup>
+//
+// </Map>
+
+// {
+//   this.renderGrid()
+// }
