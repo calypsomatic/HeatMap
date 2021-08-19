6cf0e818d4e8d76ab5da73be74fa2e4185b411bf
calypsomatic <calypsomatic@gmail.com>
2021-08-19 13:20:37 -0400

    Trying to update to react 3

--- MapView.js
+++ MapView.js
@@ -1,5 +1,5 @@
-import React, { Component, useState } from 'react';
-import { Map, TileLayer, Marker, Popup, Polygon, SVGOverlay, LayerGroup, Circle, FeatureGroup, LayersControl } from 'react-leaflet';
+import React, { Suspense, Component, useState, useEffect, useCallback } from 'react';
+import { useMapEvent, useLeafletContext, Map, TileLayer, Marker, Popup, Polygon, SVGOverlay, LayerGroup, Circle, FeatureGroup, LayersControl, MapContainer, useMap, Rectangle } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import Geolocation from '@react-native-community/geolocation';
 import L from 'leaflet';
@@ -9,165 +9,80 @@ import {storeData, getMyObject, getLocationPolygon, updateUserPolygon, removeDat
 import StreetPolygon from './StreetPolygon.js';
 import PolygonWithDate from './PolygonWithDate.js';
 
-const debug = true;
-var logger = debug ? console.log.bind(console) : function () {};
-var group = debug ? console.group.bind(console) : function () {};
-var groupEnd = debug ? console.groupEnd.bind(console) : function () {};
-
-delete L.Icon.Default.prototype._getIconUrl;
-
 //TODO how to deal with user
 const user = "testuser";
 const rad = 0.004;
+var zoom = 16;
+var markers = [];
+// var polygons = null;
+var bounds = null;
+const multiPolygon = [
+  [
+    [42.3953, -71.1257],
+    [42.3953, -71.1259],
+    [42.3954, -71.1258],
+  ],
+  [
+    [42.3953, -71.1258],
+    [42.3953, -71.1259],
+    [42.3954, -71.1259],
+  ],
+]
 
-//Gets the needed icon
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
-
-class MapView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentLocation: {lat: null, lng: null},
-      zoom: 14,
-      markers: [],
-      polygons: null,
-      bounds: null
+//   function MyComponent() {
+//     const map = useMapEvent('click', () => {
+//       map.setCenter([50.5, 30.5])
+//     })
+//     return null
+// }
+function MultiPoly(polygons) {
+  if (polygons){
+    return polygons.polygons.map((poly,i) => {
+      console.log(poly);
+                let color = poly.color ? poly.color : "dark grey";
+                let corners = poly.corners ? poly.corners : poly._polygon.corners;
+                return <Polygon fillColor = {color} positions={corners} key={poly.id} stroke={false}/>
+            });
     }
-  }
-
-  componentDidMount() {
-    //Set the current location and find nearby intersections
-    Geolocation.getCurrentPosition(position => {
-      this.setState({
-        currentLocation: { lat: position.coords.latitude, lng: position.coords.longitude },
-        bounds:     [(position.coords.longitude-rad),(position.coords.latitude-rad),(position.coords.longitude+rad),(position.coords.latitude+rad)] });
-      findExistingIntersections(user, this.state.currentLocation)
-        .then(res => {
-          group("MapView.findExistingIntersections");
-            logger(res);
-            logger(this.state);
-          this.setState({ polygons: res.polygon.concat(this.state.polygons ? this.state.polygons : [])});
-            logger(this.state);
-        //if there are nearby intersections that aren't in the db yet, create them
-        createNewIntersections(this.state.currentLocation, res)
-        .then(resnew => {
-          group("MapView.createNewIntersections");
-            logger(this.state);
-            logger(resnew);
-          // this.setState({ polygons: resnew.polygon.concat(this.state.polygons ? this.state.polygons.filter( (poly) => resnew.polygon.includes(poly)) : []),
-            this.setState({ polygons: resnew.polygon.concat(this.state.polygons ? this.state.polygons : []),
-          markers: resnew.markers ? resnew.markers.concat(this.state.markers) : this.state.markers});
-            logger(this.state);
-            groupEnd();
-        });
-
-        //Gets the polygon where you are right now
-        getLocationPolygon(this.state.currentLocation).then( res =>{
-          group("getLocationPolygon");
-            logger("location polygon:")
-            logger(res);
-          if (res && res.length){
-            let localp = res[0];
-            if (!(localp instanceof StreetPolygon)){
-              localp = toClass(localp, StreetPolygon.prototype);
-            }
-              logger(localp.id);
-            //update this polygon with current date and assign to user
-            updateUserPolygon(user, localp);
-              logger(this.state);
-            // removeData("userpolygons_"+user)
-            // removeData("polygons");
-            groupEnd();
-          }
-        })
-        groupEnd();
-      });
-
-
-   });
-
-  }
-
-  // renderPolygon(){
-  //   if (this.state.polygons){
-  //       // return <Polygon fillColor="purple" positions={this.state.polygons} stroke={false}/>
-  //       return this.state.polygons.map((poly) => (
-  //                   <Polygon fillColor = "purple" positions={poly.corners} key={poly.corners.flat()} stroke={false}/>
-  //               ))
-  //     }
-  // }
-
-  renderMarkers(){
-    if(this.state.markers){
-      // logger(this.state.markers);
-      return this.state.markers.map((marker) => (
-                  <Marker position={marker.position} key={marker.position.lat + marker.position.lng}>
-                  <Popup> {marker.label} </Popup>
-                  </Marker>
-              ))
+    else{
+      return null;
     }
-  }
+}
 
-  render() {
+function Test(){
+  const[loc, setLoc] = useState(null);
+  const[bounds, setBounds] = useState([]);
+  const [polygons, setPolygons] = useState([]);
+  const map = useMap();
 
-    if (!this.state) {
-      return <div>Loading</div>
-    }
+  useEffect(() => {
+    Geolocation.getCurrentPosition(pos => {
+        console.log(pos)
+          setLoc({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+          map.setView({ lat: pos.coords.latitude, lng: pos.coords.longitude }, 15);
+          setBounds([(pos.coords.longitude-rad),(pos.coords.latitude-rad),(pos.coords.longitude+rad),(pos.coords.latitude+rad)]);
+          findExistingIntersections(user, { lat: pos.coords.latitude, lng: pos.coords.longitude })
+            .then(res => {
+              console.log("MapView.findExistingIntersections");
+              setPolygons(res.polygon)
+            })
+          });
+  }, [])
+    return (<MultiPoly polygons={polygons}/>)
+}
 
-    //TODO: fix attribution.  given here: http://maps.stamen.com/#toner/12/37.7706/-122.3782
 
-    return (
-      <Map ref="map" center={this.state.currentLocation} zoom={this.state.zoom}>
-        <TileLayer
-          url="http://tile.stamen.com/toner/{z}/{x}/{y}.png"
-          attribution="Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL."
-        />
-        <LayersControl position="topright">
-        <LayersControl.Overlay name="Marker with popup" style={{fillColor: "blue"}}>
-        <Marker>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </LayersControl.Overlay>
-      </LayersControl>
-        <Marker position={this.state.currentLocation}>
-        <Popup> You are here: {this.state.currentLocation.lat}, {this.state.currentLocation.lng} </Popup>
-        </Marker>
-        <MultiPolygon polygons={this.state.polygons}/>
-        {
-          this.renderMarkers()
-        }
-      </Map>
-    );
-  }
+
+function MapView() {
+  return(
+    <MapContainer>
+      <TileLayer
+        url="http://tile.stamen.com/toner/{z}/{x}/{y}.png"
+        attribution="Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL."
+      />
+      <Test />
+    </MapContainer>
+    )
 }
 
 export default MapView;
-
-//
-// {this.state.markers.map((marker) => (
-//             <Marker position={marker.position} key={marker.position.lat + marker.position.lng}>
-//             <Popup> {marker.label} </Popup>
-//             </Marker>
-//         ))}
-//        var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution}),
-
-// <Map ref="map" center={this.state.currentLocation} zoom={this.state.zoom}>
-// <LayerGroup>
-// <Grid/>
-// </LayerGroup>
-//
-// </Map>
-
-// {
-//   this.renderGrid()
-// }
-
-// {
-//   this.renderPolygon()
-// }
